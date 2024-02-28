71aeefee1d6ae14368a0fd1266b0a38d2d08c75d
iancalip <ianllima@live.com>
2024-02-28 11:41:31 -0300

    sneaker working

--- rest_api/config/initializers/sneakers.rb
+++ rest_api/config/initializers/sneakers.rb
@@ -1,5 +1,2 @@
-# require "sneakers"
-# Sneakers.configure amqp: "amqp:/guest:guest@rabbitmq:5672",
-#   daemonize: false,
-#   log: `$stdout`
-# Sneakers.logger.level = Logger::INFO
+Sneakers.configure(amqp: "amqp://guest:guest@rabbitmq:5672")
+Sneakers.logger.level = Logger::INFO
