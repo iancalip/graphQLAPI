97dbc19cadd0969301d4b1e81ce102d23fac003c
iancalip <ianllima@live.com>
2024-02-29 16:44:00 -0300

    refactor and adjust policy json response structure

--- rest_api/app/controllers/policies_controller.rb
+++ rest_api/app/controllers/policies_controller.rb
@@ -1,9 +1,23 @@
 class PoliciesController < ApplicationController
   def show
     policy = Policy.find_by(id: params[:policy_id])
-    p policy
     if policy
-      render json: policy, include: [:insured, :vehicle]
+      policy_json = {
+        policy_id: policy.id,
+        data_emissao: policy.issued_date,
+        data_fim_cobertura: policy.end_coverage_date,
+        segurado: {
+          nome: policy.insured.name,
+          cpf: policy.insured.cpf
+        },
+        veiculo: {
+          marca: policy.vehicle.brand,
+          modelo: policy.vehicle.model,
+          ano: policy.vehicle.year,
+          placa: policy.vehicle.plate
+        }
+      }
+      render json: policy_json
     else
       render json: {error: "Policy not found"}, status: :not_found
     end
--- rest_api/app/workers/policy_worker.rb
+++ rest_api/app/workers/policy_worker.rb
@@ -5,23 +5,28 @@ class PolicyWorker
   from_queue "policy_created"
 
   def work(raw_policy)
-    p raw_policy
     policy_data = JSON.parse(raw_policy)
-
     policy = Policy.create!(
       issued_date: policy_data["issued_date"],
       end_coverage_date: policy_data["end_coverage_date"]
     )
+    puts "Policy created: #{policy.id}"
 
-    insured = policy_data["insured"]
-    insured_data = {policy:, name: insured["name"], cpf: insured["cpf"]}
-    Insured.find_or_create_by(insured_data)
+    Insured.find_or_create_by(
+      policy:,
+      name: policy_data["insured"]["name"],
+      cpf: policy_data["insured"]["cpf"]
+    )
+    puts "Insured associated: #{Insured.last.id}"
 
-    vehicle = policy_data["vehicle"]
-    vehicle_data = {policy:,
-                    plate: vehicle["plate"], model: vehicle["model"],
-                    brand: vehicle["brand"], year: vehicle["year"]}
-    Vehicle.find_or_create_by(vehicle_data)
+    Vehicle.find_or_create_by(
+      policy:,
+      plate: policy_data["vehicle"]["plate"],
+      model: policy_data["vehicle"]["model"],
+      brand: policy_data["vehicle"]["brand"],
+      year: policy_data["vehicle"]["year"]
+    )
+    puts "Vehicle associated: #{Vehicle.last.id}"
 
     ack!
   end
