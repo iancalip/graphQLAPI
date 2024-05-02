5addabae10ca0a2ef2fa03c30b9d860953792908
iancalip <ianllima@live.com>
2024-05-02 12:27:38 -0300

    wip policy creation via web

--- rest_api/app/workers/policy_worker.rb
+++ rest_api/app/workers/policy_worker.rb
@@ -6,33 +6,37 @@ class PolicyWorker
 
   def work(raw_policy)
     p ":::::::::::::: POLICY RECEIVED ::::::::::::::"
-    policy_data = JSON.parse(raw_policy)
+    policy_data = JSON.parse(raw_policy, symbolize_names: true)
 
-    policy = Policy.create!(
-      issued_date: policy_data["issued_date"],
-      end_coverage_date: policy_data["end_coverage_date"],
-      status: policy_data["status"],
-      payment_id: policy_data["payment_id"],
-      payment_link: policy_data["payment_link"]
-    )
-    puts "Policy created: #{policy.inspect}"
+    ActiveRecord::Base.connection_pool.with_connection do
+      policy = Policy.create!(
+        issued_date: policy_data[:issued_date],
+        end_coverage_date: policy_data[:end_coverage_date],
+        status: policy_data[:status],
+        payment_id: policy_data[:payment_id],
+        payment_link: policy_data[:payment_link]
+      )
+      puts "Policy created: #{policy.inspect}"
 
-    insured = Insured.find_or_create_by(
-      policy: policy,
-      name: policy_data["insured"]["name"],
-      cpf: policy_data["insured"]["cpf"]
-    )
-    puts "Insured associated: #{insured.inspect}"
+      insured = Insured.find_or_create_by(cpf: policy_data[:insured][:cpf]) do |i|
+        i.name = policy_data[:insured][:name]
+        i.policy = policy
+      end
 
-    vehicle = Vehicle.find_or_create_by(
-      policy:,
-      plate: policy_data["vehicle"]["plate"],
-      model: policy_data["vehicle"]["model"],
-      brand: policy_data["vehicle"]["brand"],
-      year: policy_data["vehicle"]["year"]
-    )
-    puts "Vehicle associated: #{vehicle.inspect}"
+      puts "Insured associated: #{insured.inspect}"
 
+      vehicle = Vehicle.find_or_create_by(plate: policy_data[:vehicle][:plate]) do |v|
+        v.brand = policy_data[:vehicle][:brand]
+        v.model = policy_data[:vehicle][:model]
+        v.year = policy_data[:vehicle][:year]
+        v.policy = policy
+      end
+
+      puts "Vehicle associated: #{vehicle.inspect}"
+      ack!
+    end
+  rescue => e
     ack!
+    Sneakers.logger.error "ERROR: #{e.message}"
   end
 end
