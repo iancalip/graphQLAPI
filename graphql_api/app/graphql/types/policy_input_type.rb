44c3d8e47229753fe0956b89a921674a210787f2
iancalip <ianllima@live.com>
2024-05-09 12:44:08 -0300

    webhook updating policy

--- graphql_api/app/graphql/types/policy_input_type.rb
+++ graphql_api/app/graphql/types/policy_input_type.rb
@@ -2,7 +2,7 @@ module Types
   class PolicyInputType < Types::BaseInputObject
     argument :issued_date, GraphQL::Types::ISO8601Date, required: true
     argument :end_coverage_date, GraphQL::Types::ISO8601Date, required: true
-    argument :status, Integer, required: true
+    argument :status, String, required: true
     argument :payment_id, String, required: true
     argument :payment_link, String, required: true
     argument :insured, Types::InsuredInputType, required: true
---
+++ rest_api/app/workers/payment_worker.rb
@@ -0,0 +1,24 @@
+require "json"
+
+class PaymentWorker
+  include Sneakers::Worker
+  from_queue "payment_update"
+
+  def work(msg)
+    puts "::::::::::: PAYMENT RECEIVED ::::::::::"
+    policy_data = JSON.parse(msg, symbolize_names: true)
+
+    ActiveRecord::Base.connection_pool.with_connection do
+      policy = Policy.find_by(payment_id: policy_data[:payment_id])
+      policy.update!(status: "active")
+
+      puts "######### PAYMENT UPDATED #########"
+      p policy
+
+      ack!
+    end
+  rescue => e
+    ack!
+    Sneakers.logger.error "ERROR: #{e.message}"
+  end
+end
